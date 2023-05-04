efee7ee1284584868be1efecb7597cd0a2bec5ee
Abdelhadi Amhamdi <aamhamdi@e1r3p15.1337.ma>
2023-05-04 14:00:10 +0100

    [add] parsing header file

--- parse/parsing.h
+++ parse/parsing.h
@@ -6,7 +6,7 @@
 /*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 16:20:25 by aamhamdi          #+#    #+#             */
-/*   Updated: 2023/05/04 11:40:27 by aamhamdi         ###   ########.fr       */
+/*   Updated: 2023/05/04 13:42:05 by aamhamdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 
 
 typedef enum s_boolean {false,true}	t_boolean;
+typedef enum s_type {CMD,UNK,TOKEN,ARGS}	t_type;
 
 typedef struct s_lexer
 {
@@ -26,8 +27,22 @@ typedef struct s_lexer
 	struct s_lexer	*prev;
 }	t_lexer;
 
+typedef struct s_parser
+{
+	char			**str;
+	char			*type;
+	char			*path;
+	t_boolean		is_builtin;
+	struct s_parser	*next;
+	struct s_parser	*prev;
+}	t_parser;
 
-char **args_filter(char *str);
-int	is_token(char c);
+// lexer functions
+char	**args_filter(char *str);
+int		is_token(char c);
+
+
+// parser functions
+void	parser(t_lexer *lexer_list, char **envp);
 
 #endif
