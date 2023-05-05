daddbfd64c752840da2394b578740495376856a1
Abdelhadi Amhamdi <aamhamdi@e2r3p3.1337.ma>
2023-05-05 16:53:18 +0100

    [clean] the formater

---
+++ parse/p_main.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   p_main.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/04 11:52:10 by aamhamdi          #+#    #+#             */
+/*   Updated: 2023/05/05 16:52:53 by aamhamdi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/mini_shell.h"
+
+t_parser	*formater(char *cmd, t_env *envp)
+{
+	t_lexer		*lexer_list;
+	t_parser	*parser_list;
+
+	lexer_list = lexer(cmd);
+	parser_list = parser(lexer_list, envp);
+	ft_expander(parser_list, envp);
+	return (parser_list);
+}
