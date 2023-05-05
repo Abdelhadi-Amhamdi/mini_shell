af4344e9e36e35c4972d2e892d674a9bef2cf4a6
Abdelhadi Amhamdi <aamhamdi@e2r3p3.1337.ma>
2023-05-05 16:54:33 +0100

    [fix] function protoypes

--- parse/parsing.h
+++ parse/parsing.h
@@ -6,7 +6,7 @@
 /*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 16:20:25 by aamhamdi          #+#    #+#             */
-/*   Updated: 2023/05/05 14:58:47 by aamhamdi         ###   ########.fr       */
+/*   Updated: 2023/05/05 16:22:21 by aamhamdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #ifndef PARSING_H
 # define PARSING_H
 # include "../libft/libft.h"
+# include "../env/env.h"
 
 
 typedef enum s_boolean {false,true}	t_boolean;
@@ -37,6 +38,9 @@ typedef struct s_parser
 	struct s_parser	*prev;
 }	t_parser;
 
+// main
+t_parser	*formater(char *cmd, t_env *envp);
+
 // lexer functions
 t_lexer		*lexer(char *args);
 char		**args_filter(char *str);
@@ -44,13 +48,13 @@ int			is_token(char c);
 
 
 // parser functions
-t_parser	*parser(t_lexer *list, char **envp);
+t_parser	*parser(t_lexer *list, t_env *envp);
 int			is_builtin(char *cmd);
 t_type		check_type(t_lexer *lexer_item, char *p);
 void		print_parser_list(t_parser *list);
 
 
 // expander function
-void	ft_expander(t_parser *list, char **envp);
+void		ft_expander(t_parser *list, t_env *envp);
 
 #endif
