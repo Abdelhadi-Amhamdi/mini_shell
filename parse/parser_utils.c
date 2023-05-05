1598c6f6dfc39274af65e0621a94cf4efa49af21
Abdelhadi Amhamdi <aamhamdi@e2r3p3.1337.ma>
2023-05-05 15:01:44 +0100

    [add] function to detect files

--- parse/parser_utils.c
+++ parse/parser_utils.c
@@ -6,11 +6,18 @@
 /*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 16:27:24 by aamhamdi          #+#    #+#             */
-/*   Updated: 2023/05/05 13:22:24 by aamhamdi         ###   ########.fr       */
+/*   Updated: 2023/05/05 15:00:54 by aamhamdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
+
+int	is_file(char *str)
+{
+	if (!ft_strncmp((str + (ft_strlen(str) - 4)), ".txt", 4))
+		return (1);
+	return (0);
+}
 
 t_type	check_type(t_lexer *lexer_item, char *p)
 {
@@ -24,6 +31,8 @@ t_type	check_type(t_lexer *lexer_item, char *p)
 		return (TOKEN);
 	else if (lexer_item->str[0] == '-')
 		return (ARGS);
+	else if (is_file(lexer_item->str))
+		return (FL);
 	return (UNK);
 }
 
@@ -44,10 +53,10 @@ void	print_parser_list(t_parser *list)
 		printf("path : %s\n", list->path);
 		printf("is_builtin : %s\n", list->is_builtin ? "true" : "false");
 		printf("type : %s\n", (list->type == 0) ? "CMD" \
-		: ((list->type == 1)) ? "UNK" \
-		: ((list->type == 2) ? "TOKEN" : ((list->type == 3) ? "ARGS" \
-		: ((list->type == 4) ? "VAR" : "Q"))));
-		printf("---------------------------\n");
+		: ((list->type == 1)) ? "UNK" : ((list->type == 2) \
+		? "TOKEN" : ((list->type == 3) ? "ARGS" : ((list->type == 4) \
+		? "VAR" : ((list->type == 5)? "FILE" : "Q")))));
+		printf("----------\n");
 		list = list->next;
 	}
 }
