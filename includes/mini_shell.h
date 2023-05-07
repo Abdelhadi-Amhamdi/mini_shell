fbfe6a87605047a9106e533f21fa2305cd243578
Abdelhadi Amhamdi <aamhamdi@e2r4p1.1337.ma>
2023-05-07 17:11:09 +0100

    [update] main header file

--- includes/mini_shell.h
+++ includes/mini_shell.h
@@ -6,19 +6,21 @@
 /*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 15:13:34 by aamhamdi          #+#    #+#             */
-/*   Updated: 2023/05/05 21:38:53 by aamhamdi         ###   ########.fr       */
+/*   Updated: 2023/05/07 16:15:01 by aamhamdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINI_SHELL_H
 # define MINI_SHELL_H
 # include "../parse/parsing.h"
+# include "../builtins/builtins.h"
 # include "../env/env.h"
-# include "../libft/libft.h"
+# include "../libs/libft/libft.h"
+# include "../libs/ft_printf/ft_printf.h"
 # include <readline/readline.h>
 # include <readline/history.h>
 
-int	executer(t_parser *cmds_list);
+int	executer(t_parser *cmds_list, t_env *env);
 
 
 
