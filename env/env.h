611a2566a8ea0d00016ac6d7ef6e2a4b341c7527
Abdellah Agouzoul <aagouzou@e2r2p2.1337.ma>
2023-05-09 14:48:03 +0100

    [edit] env header

--- env/env.h
+++ env/env.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aagouzou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 15:20:59 by aamhamdi          #+#    #+#             */
-/*   Updated: 2023/05/07 16:15:28 by aamhamdi         ###   ########.fr       */
+/*   Updated: 2023/05/09 14:43:13 by aagouzou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ typedef struct s_env
 t_env	*ft_new_node(char *key, char *val);
 void	ft_add_back_env(t_env **list, t_env *item);
 t_env	*get_env_vars(char **envp);
-
+void	formate_env_item(char **key, char **val, char *item);
+int ft_search(char *item, char tofind);
 
 #endif
