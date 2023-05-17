1a89707fb7077b69b4945a0089a70aec2d9bdb3f
Abdelhadi Amhamdi <aamhamdi@e2r8p4.1337.ma>
2023-05-17 15:54:35 +0100

    [add] function to check operation syntax

--- parse/expander.c
+++ parse/expander.c
@@ -6,7 +6,7 @@
 /*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 11:47:42 by aamhamdi          #+#    #+#             */
-/*   Updated: 2023/05/16 13:30:23 by aamhamdi         ###   ########.fr       */
+/*   Updated: 2023/05/17 15:53:00 by aamhamdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,8 @@
 // 	}
 // }
 
+#define _ERR_MSG "shell : parse error near"
+
 int check_qoutes(t_lexer *lexer_list)
 {
 	t_lexer		*tmp;
@@ -65,16 +67,73 @@ int check_qoutes(t_lexer *lexer_list)
 	return (0);
 }
 
+int check_opeators(t_lexer *op)
+{
+	if (!op->next || op->next->type == CP || op->next->type == SQ \
+	|| op->next->type == DQ || (op->next->is_oper && op->next->type != RDIR))
+	{
+		printf("%s `%s'\n", _ERR_MSG, op->str);
+		return (1);
+	}
+	if (op->prev->type == OP || op->prev->type == SQ \
+	|| op->prev->type == DQ || op->prev->is_oper)
+	{
+		printf("%s `%s'\n", _ERR_MSG, op->str);
+		return (1);
+	}
+	return (0);
+}
+
+int check_pth(t_lexer *pt)
+{
+	(void)pt;
+	return (0);
+}
+
+int check_redir(t_lexer *rdir)
+{
+	(void)rdir;
+	return (0);
+}
+
+int syntax_analyzer(t_lexer *list)
+{
+	t_lexer *tmp;
+	int res;
+
+	tmp = list;
+	res = 0;
+	while (tmp)
+	{
+		if (tmp->is_oper)
+			res += check_opeators(tmp);
+		else if (tmp->type == OP || tmp->type == CP)
+			res += check_pth(tmp);
+		else if (tmp->type == RDIR)
+			res += check_redir(tmp);
+		if (!ft_strncmp(tmp->str, ";;", ft_strlen(tmp->str)))
+			res++;
+		if (res)
+			return 1;
+		tmp = tmp->next;
+	}
+	return (0);
+}
+
 t_lexer *ft_expander(t_lexer *list, t_env *env)
 {
 	// check quotes;
 	(void) env;
 	if (check_qoutes(list))
 		return (NULL);
+
+	// syntax analyzer
+	// if (syntax_analyzer(list))
+	// 	return (NULL);
 	// create blocks
 	
 	// expand vars
 	
 	// clean list;	
-	return (NULL);
+	return (list);
 }
