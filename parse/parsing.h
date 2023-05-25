668eed2385fb055c4f934db24262a4255bc61614
Abdelhadi Amhamdi <aamhamdi@e2r9p14.1337.ma>
2023-05-25 16:56:37 +0100

    [add] wildcard handler to the expander

--- parse/parsing.h
+++ parse/parsing.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aagouzou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 16:20:25 by aamhamdi          #+#    #+#             */
-/*   Updated: 2023/05/24 13:40:16 by aagouzou         ###   ########.fr       */
+/*   Updated: 2023/05/25 10:55:07 by aamhamdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,7 @@ typedef enum s_type
 	CP,
 	UNK,
 	EMPTY,
+	WILDCARD,
 	HEREDOC
 }					t_type;
 
@@ -91,6 +92,8 @@ typedef struct s_app
 	t_tree		*ast_tree;
 	char		*cmd;
 	t_env		*env_list;
+	int			in;
+	int			out;
 }	t_app;
 
 #define _ERR_MSG "shell : parse error near"
