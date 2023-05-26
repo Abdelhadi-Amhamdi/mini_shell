df7c4f836884a4dfe51af977aa999a3cc3426d90
Abdelhadi Amhamdi <aamhamdi@e2r12p5.1337.ma>
2023-05-26 15:54:36 +0100

    [add] signal handler

--- src/main.c
+++ src/main.c
@@ -2,6 +2,9 @@
 #include "../includes/mini_shell.h"
 #include <sys/types.h>
 #include <sys/wait.h>
+# include <signal.h>
+
+t_app *app;
 
 void clean_data(t_app *app)
 {
@@ -9,8 +12,6 @@ void clean_data(t_app *app)
 	app->cmd = NULL;
 	app->lexer_list = NULL;
 	app->parser_list = NULL;
-	app->in = 0;
-	app->out = 1;
 }
 
 void print_ast(t_tree *root)
@@ -43,59 +44,94 @@ void	exec_builtin(t_tree	*cmd, t_env	**env)
 int executer(t_tree *root, t_env *env)
 {
 	if (!root)
-		return (-11);
+		return (-1);
 	if (root->type == CMD)
 		return (run_cmd(root, &env));
 	else if (root->type == PIPE)
 		return (run_pipeline(root, 0, 1));
 	else if (root->type == RDIR || root->type == APND)
-		run_rdir(root);
+		return (run_rdir(root));
 	else if (root->type == HEREDOC)
-		herdoc(root->right->str);
+		return (herdoc(root->right->str, app));
 	else if (root->type == AND || root->type == OR)
 		return (run_connectors(root));
-	return (-11);
+	return (-1);
 }
 
-void test(int sigtype)
+void ft_free_heredoc_list(t_h_list **list)
 {
-	if (sigtype == SIGINT)
+	t_h_list *tmp;
+	t_h_list *next;
+
+	tmp = *list;
+	while (tmp)
 	{
-		int s = 0;
-		printf("%d\n", WEXITSTATUS(s));
-		// rl_on_new_line();
-        // rl_replace_line("", 0);
-        // rl_redisplay();
+		next = tmp->next;
+		free(tmp);
+		tmp = next;
 	}
+	*list = NULL;
+}
+
+void test(int type)
+{
+
+	if (type == SIGINT)
+	{
+		if (app->lexer_list)
+			ft_free_lexer_list(&app->lexer_list);
+		if (app->parser_list)
+			ft_free_parser_list(&app->parser_list);
+		free(app->cmd);
+		// destroy the tree;
+		ft_free_heredoc_list(&app->herdoc_list);
+		ft_putstr_fd("\n", STDOUT_FILENO);
+		rl_on_new_line();
+        rl_replace_line("", 0);
+        rl_redisplay();
+	}
+}
+
+void init(t_app *app, char **env)
+{
+	app->status = 0;
+	app->cmd = NULL;
+	app->ast_tree = NULL;
+	app->lexer_list = NULL;
+	app->parser_list = NULL;
+	app->herdoc_list = NULL;
+	app->env_list = get_env_vars(env);
+	signal(SIGINT, test);
+	signal(SIGQUIT, SIG_IGN);
 }
 
 int main(int ac, char **av, char **envp)
 {
-	t_app *app;
-
 	(void)ac;
 	(void)av;
 	app = malloc(sizeof(t_app));
 	if (!app)
 		return (0);
-	// signal(SIGINT, test);
-	app->env_list = get_env_vars(envp);
+	init(app, envp);
 	while (1)
 	{
 		clean_data(app);
-		app->cmd = NULL;
 		app->cmd = readline("mini_sh-1.0$ ");
 		if (!app->cmd)
-			exit (1);
+			break ;
 		if (app->cmd[0])
 		{
 			app->ast_tree = formater(app);
 			if(app->ast_tree)
-				executer(app->ast_tree, app->env_list);
-			// printTree(app->ast_tree);
+			{
+				// printTree(app->ast_tree);
+				app->status = executer(app->ast_tree, app->env_list);
+				// destroy tree;
+			}
 			add_history(app->cmd);
 			free(app->cmd);
 		}
 	}
+	// clean all;
 	return (0);
 }
