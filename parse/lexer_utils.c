48de709dd65bff3d2ae9b932c8457bdceabf1082
Abdellah Agouzoul <aagouzou@e2r12p5.1337.ma>
2023-05-28 16:36:59 +0100

    handle variable case

--- parse/lexer_utils.c
+++ parse/lexer_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lexer_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aagouzou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 17:01:56 by aamhamdi          #+#    #+#             */
-/*   Updated: 2023/05/28 13:37:18 by aamhamdi         ###   ########.fr       */
+/*   Updated: 2023/05/28 15:04:01 by aagouzou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -188,18 +188,16 @@ void	print_token_list(t_lexer *head)
 	cur = head;
 	while (cur != NULL)
 	{
-		printf("String: :%s:\n", cur->str);
-		printf("Is op: %s\n", cur->is_oper ? "true" : "false");
-		printf("type: %u\n", cur->type);
-		printf("path	: %s\n",cur->path);
-		// printf("is_builtin	: %s\n",cur->is_builtin ? "true" : "false");
+		printf("STR : [%s]\n", cur->str);
+		printf("IS_O: [%s]\n", cur->is_oper ? "true" : "false");
+		printf("TYPE: [%u]\n", cur->type);
+		printf("PATH: [%s]\n",cur->path);
+		// printf("is_|bu|iltin	: %s\n",cur->is_builtin ? "true" : "false");
 		// printf("type : %s\n", (cur->type == 0) ? "CMD" : (cur->type == 1) ? "PIPE" \
 		//  : (cur->type == 2) ? "RDIR" : (cur->type == 3) ? "APND" : (cur->type == 4) ? "AND" : (cur->type == 5) ? "OR" : (cur->type == 6) ? "ARGS" : (cur->type == 7) ? "VAR": (cur->type == 8) ? "FILE": (cur->type == 9) ? "SQ": (cur->type == 10) ? "DQ"\
 		//  : (cur->type == 11) ? "OP": ((cur->type == 12) ? "CP": (cur->type == 15) ? "wildcard" : "UNK"));
 		// printf("type index %u\n",cur->type);
-		printf("-----------\n");
 		cur = cur->next;
+		puts("------------------------------");
 	}
-	printf("-------------------------\n");
-	printf("-------------------------\n");
 }
