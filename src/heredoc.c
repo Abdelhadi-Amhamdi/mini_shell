cfb4bbf169350540215f312085c4abf903a7298c
Abdelhadi Amhamdi <aamhamdi@e2r8p13.1337.ma>
2023-06-07 17:30:54 +0100

    [add] heredoc handler in lexer

--- src/heredoc.c
+++ src/heredoc.c
@@ -6,14 +6,131 @@
 /*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 13:17:19 by aamhamdi          #+#    #+#             */
-/*   Updated: 2023/06/06 16:46:47 by aamhamdi         ###   ########.fr       */
+/*   Updated: 2023/06/07 17:28:31 by aamhamdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/mini_shell.h"
 #include "../libs/gnl/get_next_line.h"
 
-# define DOC_FILE "heredoc_file"
+void heredoc_handler(t_lexer **list)
+{
+	t_lexer *tmp;
+
+	tmp = *list;
+	while (tmp)
+	{
+		if (tmp->type == HEREDOC)
+		{
+			// if (tmp->prev)
+			// 	tmp->prev->next = heredoc_helper(tmp);
+			if (!tmp->prev)
+				*list = heredoc_helper(tmp);
+			else
+				heredoc_helper(tmp);
+		}
+		tmp = tmp->next;
+	}	
+}
+
+t_lexer *get_hlast_token(t_lexer *root)
+{
+	t_lexer *tmp;
+
+	tmp = root->prev;
+	while (tmp && !tmp->is_oper)
+	{
+		if (tmp->type == CMD)
+			return (tmp);
+		tmp = tmp->prev;
+	}
+	return (NULL);
+}
+
+t_lexer *get_next_token(t_lexer *root)
+{
+	t_lexer *tmp;
+
+	tmp = root->next;
+	while (tmp && !tmp->is_oper)
+	{
+		if (tmp->type == CMD)
+			return (tmp);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
+
+char *start_heredoc(t_lexer *node)
+{
+	char	*line;
+	char	*del;
+	char 	*file_name;
+	
+	file_name = ft_strjoin(HEREDOC_FILENAME, ft_itoa(node->id));
+	del = node->next->str;
+	app->hdoc_fd = open(file_name, O_CREAT | O_RDWR, 0644);
+	if (app->hdoc_fd == -1)
+		return (NULL);
+	write(0, "> ", 2);
+	line = get_next_line(0);
+	while (1)
+	{
+		ft_putstr_fd(line, app->hdoc_fd);
+		free(line);
+		write(0, "> ", 2);
+		line = get_next_line(0);
+		if (!line || !ft_strncmp(line, del, (ft_strlen(line) - 1)))
+			break ;
+	}
+	free (line);
+	close(app->hdoc_fd);
+	return (file_name);
+}
+
+t_lexer  *heredoc_helper(t_lexer *root)
+{
+	t_lexer *next;
+	t_lexer *prev;
+	t_lexer *tmp;
+	char *file_name;
+
+	prev = get_hlast_token(root);
+	next = get_next_token(root);
+	file_name = start_heredoc(root);
+	if (next && prev)
+	{
+		unlink(file_name);
+		prev->next = next;
+	}
+	else
+	{
+		tmp = ft_nodedup(root);
+		tmp->str = file_name;
+		tmp->path = file_name;
+		tmp->type = FL;
+		if (next)
+		{
+			tmp->next = next->next;
+			next->prev = NULL;
+			next->next = tmp;
+			del_node(root->next);
+			del_node(root);
+			return (next);
+		}
+		else if (prev)
+		{
+			prev->next = tmp;
+			tmp->prev = prev;
+			tmp->next = root->next->next;
+		}
+	}
+	if (!next && !prev)
+		unlink(file_name);
+	del_node(root->next);
+	del_node(root);
+	return (NULL);
+}
 
 int	herdoc(t_tree *hrd)
 {
