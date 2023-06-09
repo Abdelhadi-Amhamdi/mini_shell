51fd42d0bab07c69f09bba9ccb9a0b5a381d51b6
Abdellah Agouzoul <aagouzou@e2r13p5.1337.ma>
2023-06-09 14:19:46 +0100

    [fix] echo if multi spaces between args

--- builtins/ft_echo.c
+++ builtins/ft_echo.c
@@ -3,16 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   ft_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aagouzou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 15:08:05 by aagouzou          #+#    #+#             */
-/*   Updated: 2023/06/05 15:17:26 by aamhamdi         ###   ########.fr       */
+/*   Updated: 2023/06/08 16:31:00 by aagouzou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* ************************************************************************** */
 
 #include "../includes/mini_shell.h"
+
+int is_all_space(char *s)
+{
+	int i;
+
+	i = 0;
+	while(s[i])
+	{
+		if(!is_space(s[i]))
+			return (0);
+		i++;
+	}
+	return(1);
+}
 
 int	check_new_line(char *arg)
 {
@@ -32,7 +46,10 @@ int	ft_echo(t_tree *node)
 	new_line = check_new_line(node->args[i]);
 	while (node->args[i])
 	{
-		printf("%s", node->args[i]);
+		if(is_all_space(node->args[i]))
+			printf(" ");
+		else
+			printf("%s", node->args[i]);
 		i++;
 	}
 	if (new_line)
--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aagouzou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 16:49:28 by aamhamdi          #+#    #+#             */
-/*   Updated: 2023/06/07 22:11:56 by aamhamdi         ###   ########.fr       */
+/*   Updated: 2023/06/09 14:17:22 by aagouzou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -108,7 +108,7 @@ int main(int ac, char **av, char **envp)
 			if(ast_tree)
 			{
 				// printTree(ast_tree);
-				executer(ast_tree);
+				// executer(ast_tree);
 				destroy_ast_tree(ast_tree);
 			}
 			add_history(cmd);
