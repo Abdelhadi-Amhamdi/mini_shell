2e208372dee1e7b9eebfb701a4d763d1279d456f
Abdellah Agouzoul <aagouzou@e2r9p12.1337.ma>
2023-06-11 18:10:22 +0100

    [Edit] fix ft_pwd if rm current working dir

--- builtins/ft_pwd.c
+++ builtins/ft_pwd.c
@@ -3,21 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   ft_pwd.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aagouzou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 13:25:44 by aamhamdi          #+#    #+#             */
-/*   Updated: 2023/06/04 16:05:32 by aamhamdi         ###   ########.fr       */
+/*   Updated: 2023/06/11 18:08:10 by aagouzou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/mini_shell.h"
 
-int	ft_pwd(void)
+char *get_cwd(t_env	*envp)
+{
+	t_env	*tmp;
+
+	tmp = envp;
+	while(tmp)
+	{
+		if(!ft_strncmp(tmp->key,"PWD",3))
+			break;
+		tmp = tmp->next;
+	}
+	return(tmp->value);
+}
+
+int	ft_pwd(t_env	*envp)
 {
 	char	*path;
 
 	path = getcwd(NULL, 0);
 	if(path)
 		ft_putendl_fd(path, 1);
+	else
+		ft_putendl_fd(get_cwd(envp),1);
 	return (0);
 }
