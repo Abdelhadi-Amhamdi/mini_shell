44c9be0a608fc1df3023c87fb62a52083e09ad1e
Abdellah Agouzoul <aagouzou@e3r9p1.1337.ma>
2023-07-20 20:12:36 +0100

    clean non used function

--- parse/lexer_utils2.c
+++ parse/lexer_utils2.c
@@ -6,42 +6,42 @@
 /*   By: aagouzou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 21:45:53 by aamhamdi          #+#    #+#             */
-/*   Updated: 2023/06/17 11:17:47 by aagouzou         ###   ########.fr       */
+/*   Updated: 2023/07/19 20:45:15 by aagouzou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
-int	is_special_char(char c)
-{
-	if (c == '|' || c == '<' || c == '>' || c == '&' || c == '(' || c == ')'
-		|| c == '*')
-		return (1);
-	return (0);
-}
+// int	is_special_char(char c)
+// {
+// 	if (c == '|' || c == '<' || c == '>' || c == '&' || c == '(' || c == ')'
+// 		|| c == '*')
+// 		return (1);
+// 	return (0);
+// }
 
-int	is_container(char c)
-{
-	if (c == '\'' || c == '"')
-		return (1);
-	return (0);
-}
+// int	is_container(char c)
+// {
+// 	if (c == '\'' || c == '"')
+// 		return (1);
+// 	return (0);
+// }
 
-int	check_op_next(char *str, int i)
-{
-	if ((is_special_char(str[i]) && str[i + 1] != ' ') && (str[i] != str[i + 1]
-			|| !is_operator(str[i + 1])))
-		return (1);
-	return (0);
-}
+// int	check_op_next(char *str, int i)
+// {
+// 	if ((is_special_char(str[i]) && str[i + 1] != ' ') && (str[i] != str[i + 1]
+// 			|| !is_operator(str[i + 1])))
+// 		return (1);
+// 	return (0);
+// }
 
-int	check_op_prev(char *str, int i)
-{
-	if ((is_special_char(str[i]) && str[i - 1] != ' ') && (str[i] != str[i - 1]
-			|| !is_operator(str[i - 1])))
-		return (1);
-	return (0);
-}
+// int	check_op_prev(char *str, int i)
+// {
+// 	if ((is_special_char(str[i]) && str[i - 1] != ' ') && (str[i] != str[i - 1]
+// 			|| !is_operator(str[i - 1])))
+// 		return (1);
+// 	return (0);
+// }
 
 int	is_operator(char c)
 {
